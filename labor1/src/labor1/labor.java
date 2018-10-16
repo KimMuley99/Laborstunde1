524cc0772ab93d75f1bab33bf6e9f49551ea0279
KimMuley99 <44192967+KimMuley99@users.noreply.github.com>
2018-10-16 11:04:11 +0200

    Update labor.java

--- labor1/src/labor1/labor.java
+++ labor1/src/labor1/labor.java
@@ -5,6 +5,7 @@ public class labor {
 	public static void main(String[] args) {
 		// TODO Automatisch generierter Methodenstub
  System.out.println("Hello");
+		 System.out.println("Hello Germany");
 	}
 
 }
